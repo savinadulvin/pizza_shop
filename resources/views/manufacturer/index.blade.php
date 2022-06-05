7c9036d661fe7d68913c9a8c168d769ba7ec62a4
savinadulvin <savinadulvin3@gmail.com>
2022-06-05 19:02:30 +0530

    third

--- resources/views/manufacturer/index.blade.php
+++ resources/views/manufacturer/index.blade.php
@@ -16,7 +16,7 @@
                             <th scope="col">Name</th>
                             <th scope="col">Description</th>
                             <th scope="col">Status</th>
-                            {{-- <th scope="col">Actions</th> --}}
+                            
                         </tr>
                     </thead>
                     <tbody>
